5e59f3f1bb6a0ca3f3e57600867b2163036be423
Aastha Vashistha <2022btechaidsaastha12952@poornima.edu.in>
2024-06-03 22:31:25 +0530

    Modified data_management.py and added preprocessors.py

--- src/preprocessing/data_management.py
+++ src/preprocessing/data_management.py
@@ -2,31 +2,34 @@ import os
 import pandas as pd
 import pickle
 
-from src.config import config 
+from src.config import config
 
 
-def load_dataset(file_name) :
+def load_dataset(file_name):
 
-    file_path =  os.path.join(config.DATAPATH, file_name)
-    #"/src/datasets/<file_name>"       Eg output 
+    file_path = os.path.join(config.DATAPATH,file_name)
+    #"/src/datasets/file_name"
 
     data = pd.read_csv(file_path)
 
     return data
 
-def save_model(theta0,thetaa):
 
-    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH, "two_input_xor_nn.pkl")
+def save_model(theta0,theta):
+
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,"two_input_xor_nn.pkl")
 
     with open(pkl_file_path,"wb") as file_handle:
-        file_handle.dump({"params":{"baises": theta0, "weights": thetaa}, "activations":config.f})
+        pickle.dump({"params":{"biases":theta0,"weights":theta},"activations":config.f}, file_handle)
+
+    print("Saved model with file name {} at {}".format("two_input_xor_nn.pkl",config.SAVED_MODEL_PATH))
+
 
 def load_model(file_name):
 
-    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH, file_name)
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,file_name)
 
-    with open(pkl_file_path, "rb") as file_handle:
-            
-            trained_params = file_handle.load()
+    with open(pkl_file_path,"rb") as file_handle:
+        loaded_model = pickle.load(file_handle)
 
-            return trained_params["biases"], trained_params["weights"]
+    return loaded_model
